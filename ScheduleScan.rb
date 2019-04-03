dae233a80d5f4792f7465ca393ffb8365d0397cb
Hunter Wallace <hunterjwallace@gmail.com>
2019-04-03 11:42:49 -0400

    Clean Up

--- ScheduleScan.rb
+++ ScheduleScan.rb
@@ -13,8 +13,7 @@ class Scraper
     def get_seats
       avSeats = parse_page.css("tr").css(".dddefault").css("td:nth-child(4)").children.map { |seats| seats.text }.compact
     end
-  
-
+    
     classes = { "Web dev" => "https://banner.bsu.edu/ssbprod/bwckschd.p_disp_detail_sched?term_in=201910&crn_in=30024",
                 "Theory of Comp" => "https://banner.bsu.edu/ssbprod/bwckschd.p_disp_detail_sched?term_in=201910&crn_in=21805",
                 "Programming Languages" => "https://banner.bsu.edu/ssbprod/bwckschd.p_disp_detail_sched?term_in=201910&crn_in=30020",
@@ -22,9 +21,9 @@ class Scraper
                 "Business Info Systems" => "https://banner.bsu.edu/ssbprod/bwckschd.p_disp_detail_sched?term_in=201910&crn_in=24910"
               }
     
-    puts "--------------------------------------------"
-    puts "---\t Class Seat Tracker - Hunter W \t---"
-    puts "--------------------------------------------"
+    puts "---------------------------------------------------"
+    puts "---\t Class Seat Tracker - Fall 2019 \t---"
+    puts "---------------------------------------------------"
     puts
 
     classes.each do |key , value|
@@ -32,7 +31,7 @@ class Scraper
       seat = scraper.get_seats
       seat.pop #Removes waitlist data
       
-      puts "Class: #{key} | Seats open (Open | Waitlist): #{seat}"
+      puts "Class: #{key} | Seats open: #{seat}"
       puts "-----------------------------------------"
     end
 
